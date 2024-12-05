6cea34df035042f514e34aa142a52bff3a004ca8
SageMik <sagemik@163.com>
2024-12-05 23:43:47 +0800

    通过 sqlite-ohos.dart 成功实现 HarmonyOS 适配

--- example/lib/sqlite3.dart
+++ example/lib/sqlite3.dart
@@ -1,7 +1,5 @@
 import 'dart:ffi';
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter_platform_utils/flutter_platform_utils.dart';
 import 'package:sqlite3/sqlite3.dart' as s3;
 import 'package:sqlite3/sqlite3.dart' hide sqlite3;
