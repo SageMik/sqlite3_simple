e50e7d58f7d53d540eacca57c0da8e01823dd077
SageMik <sagemik@163.com>
2025-02-16 22:20:40 +0800

    将数据库实现抽象为接口，为 sqfite_common_ffi 实现做准备

---
+++ example/lib/data/db_manager.dart
@@ -0,0 +1,8 @@
+import 'package:sqlite3_simple_example/data/main_table_dao.dart';
+
+abstract class IDbManager<TDao extends IMainTableDao> {
+  abstract final TDao dao;
+
+  /// 初始化 Simple 分词器，并将结巴分词字典文件保存到本地
+  Future<void> init();
+}
