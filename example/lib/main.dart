40fa4e24edf33688764b5ce323ffc801d60f3be1
SageMik <sagemik@163.com>
2025-08-08 21:36:59 +0800

    修复水波纹特效超出列表的问题

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -221,58 +221,60 @@ class _MyAppState<T> extends State<MyApp> {
       itemCount: results!.length,
       itemBuilder: (context, index) {
         final r = results![index];
-        return InkWell(
-          onTap: () => showDialog(
-              context: context, builder: (context) => buildDialog(context, r)),
-          child: Ink(
-            padding: const EdgeInsets.only(
-                left: P.middle,
-                right: P.middle,
-                top: P.extraSmall,
-                bottom: P.extraSmall),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  r.idFormatted,
-                  style: const TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.w600,
-                      height: 1,
-                      letterSpacing: -1,
-                      fontFeatures: [FontFeature.tabularFigures()]), // 数字等宽
-                ),
-                const SizedBox(width: P.small),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ExtendedText(r.title,
-                                specialTextSpanBuilder: highlightTextBuilder,
-                                style: const TextStyle(fontSize: 20)),
-                            ExtendedText(r.content,
-                                specialTextSpanBuilder: highlightTextBuilder),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text("${r.insertDate}",
-                              textAlign: TextAlign.end,
-                              style: const TextStyle(
-                                  fontSize: 12, color: Colors.grey)),
-                        ],
-                      )
-                    ],
+        return Material( // 水波纹特效超出列表：https://github.com/flutter/flutter/issues/73315
+          child: InkWell(
+            onTap: () => showDialog(
+                context: context, builder: (context) => buildDialog(context, r)),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: P.middle,
+                  right: P.middle,
+                  top: P.extraSmall,
+                  bottom: P.extraSmall),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    r.idFormatted,
+                    style: const TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                        height: 1,
+                        letterSpacing: -1,
+                        fontFeatures: [FontFeature.tabularFigures()]), // 数字等宽
                   ),
-                ),
-              ],
+                  const SizedBox(width: P.small),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ExtendedText(r.title,
+                                  specialTextSpanBuilder: highlightTextBuilder,
+                                  style: const TextStyle(fontSize: 20)),
+                              ExtendedText(r.content,
+                                  specialTextSpanBuilder: highlightTextBuilder),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text("${r.insertDate}",
+                                textAlign: TextAlign.end,
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey)),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
