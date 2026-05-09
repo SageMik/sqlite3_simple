dd4d13f1520f38cffc54c6b21b6178d2d9b4d1a9
SageMik <sagemik@163.com>
2026-05-09 14:16:49 +0800

    优化下载脚本

--- scripts/download_release.dart
+++ scripts/download_release.dart
@@ -4,17 +4,18 @@ import 'dart:io';
 
 import 'package:archive/archive.dart';
 
-const version = "2.0.0+1";
+const version = "2.1.0";
 
-void main() async {
-  Directory.current = File(Platform.script.toFilePath()).parent.parent.path;
+void main(List<String> args) async {
+  const r = "../";
+  final localZipPath = args.isNotEmpty ? args.first : null;
 
   final file2versionUpdated = {
-    File('android/build.gradle'): (
+    File('$r/android/build.gradle'): (
       RegExp(r'simple-native-android:[^"]+'),
       'simple-native-android:$version',
     ),
-    File('linux/CMakeLists.txt'): (
+    File('$r/linux/CMakeLists.txt'): (
       RegExp(r'GIT_TAG Nv.*'),
       'GIT_TAG Nv$version',
     ),
@@ -31,30 +32,42 @@ void main() async {
   );
   print("开始下载 Simple 原生库：$releaseUrl");
   final remote2local = {
-    'windows/simple.dll': File('windows/simple.dll'),
-    'macos/libsimple.dylib': File('darwin/libsimple.dylib'),
-    'ios/libsimple.a': File('darwin/libsimple.a'),
-    'ios/libsimple-simulator.a': File('darwin/libsimple-simulator.a'),
+    'windows/simple.dll': File('$r/windows/simple.dll'),
+    'macos/libsimple.dylib': File('$r/darwin/libsimple.dylib'),
+    'ios/libsimple.a': File('$r/darwin/libsimple.a'),
+    'ios/libsimple-simulator.a': File('$r/darwin/libsimple-simulator.a'),
   };
   final client = HttpClient()..findProxy = HttpClient.findProxyFromEnvironment;
   try {
-    final request = await client.getUrl(releaseUrl);
-    final response = await request.close();
-    if (response.statusCode == 200) {
-      final bytes = <int>[];
-      await for (final chunk in response) {
-        bytes.addAll(chunk);
+    final bytes = <int>[];
+    if (localZipPath != null) {
+      final localZip = File(localZipPath);
+      if (!await localZip.exists()) {
+        throw Exception("本地文件不存在：$localZipPath");
       }
-      final archive = ZipDecoder().decodeBytes(bytes);
-      for (final file in archive) {
-        if (!file.isFile) {
-          continue;
+      print("使用本地文件：$localZipPath");
+      bytes.addAll(await localZip.readAsBytes());
+    } else {
+      print("开始下载 Simple 原生库：$releaseUrl");
+      final request = await client.getUrl(releaseUrl);
+      final response = await request.close();
+      if (response.statusCode == 200) {
+        await for (final chunk in response) {
+          bytes.addAll(chunk);
         }
-        final localFile = remote2local[file.name.replaceAll('\\', '/')];
-        if (localFile != null) {
-          await localFile.parent.create(recursive: true);
-          await localFile.writeAsBytes(file.content as List<int>);
-        }
+      } else {
+        throw Exception("响应状态码错误：${response.statusCode}");
+      }
+    }
+    final archive = ZipDecoder().decodeBytes(bytes);
+    for (final file in archive) {
+      if (!file.isFile) {
+        continue;
+      }
+      final localFile = remote2local[file.name.replaceAll('\\', '/')];
+      if (localFile != null) {
+        await localFile.parent.create(recursive: true);
+        await localFile.writeAsBytes(file.content as List<int>);
       }
     }
     print("更新完成");
