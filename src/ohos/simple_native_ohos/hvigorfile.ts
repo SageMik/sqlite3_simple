7eeb3884b3b9ddebca7d6542c42bc97ed59dbd99
SageMik <sagemik@163.com>
2026-05-26 23:32:12 +0800

    通过 hvigor-ohpm-publish Hvigor 插件发布鸿蒙原生库

--- src/ohos/simple_native_ohos/hvigorfile.ts
+++ src/ohos/simple_native_ohos/hvigorfile.ts
@@ -1,6 +1,7 @@
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
+import { ohpmPublishPlugin } from 'hvigor-ohpm-publish'
 
 export default {
     system: harTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+    plugins:[ohpmPublishPlugin()]         /* Custom plugin to extend the functionality of Hvigor. */
 }
